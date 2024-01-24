d6d444b07e71d1e987e1b66654892458a44c570a
gistaimlb <rravikumar34@gmail.com>
2024-01-24 16:54:54 +0530

    Day-03 Updated

---
+++ Day-03/Packages and Modules/employee.py
@@ -0,0 +1,25 @@
+z = 25
+'''
+module by using variable name
+'''
+
+def employeedetails(j):
+    '''
+        Employee Details
+    '''
+    print(f"Employee Name: {j}")
+    return
+
+class Employee():
+    ' Sample Example for Employee Class'
+    t = "Suresh"
+    
+    def __init__(self,z):
+        self.nm=z
+    
+    def empdetails(self):
+        'Example for method'
+        print(f"Employee details are: {self.nm}")
+        return
+        
+
